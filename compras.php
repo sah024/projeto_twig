0481695475fd49f9ac8e1be4544b867bed245a97
sah024 <sarah.tumelero.da.silveira@gmail.com>
2025-03-18 08:52:36 -0300

    Terceiro commit
    aula 3 - listagem de compras

--- compras.php
+++ compras.php
@@ -1,6 +1,14 @@
 <?php
 //compras.php
 require_once('twig_carregar.php');
+require('inc/banco.php');
+
+//Busca as compras no banco
+$dados = $pdo->query('SELECT * FROM compras');
+
+$comp = $dados->fetchAll(PDO::FETCH_ASSOC);
+
 echo $twig->render('compras.html', [
     'titulo' => 'Compras',
+    'compras' => $comp,
 ]);
---
+++ inc/banco.php
@@ -0,0 +1,4 @@
+<?php
+//lembrete: não se deve salvar senha diretamente no arquivo (da maneira como estamos fazendo aqui)
+
+$pdo = new PDO('mysql:host=localhost;dbname=3info', 'root', '');
